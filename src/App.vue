409d093c62608e4280e6c53274ccbd7c86e50572
jaromeaviles <jaromeaviles@gmail.com>
2024-02-16 16:09:56 +0800

    Added modal

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to Vue!</h1>
+  <br />
+  <button @click="toggleModal">Show Modal 1</button>
+  <button @click="toggleModalTwo">Show Modal 2</button>
+
+  <teleport to="#modals" v-if="isShowModal">
+    <Modal @close="toggleModal">
+      <h2>Modal 1</h2>
+      <p>Welcome to Modal 1</p>
+
+      <template v-slot:link>
+        <a href="#">Signup</a>
+      </template>
+    </Modal>
+  </teleport>
+
+<teleport to="#modals" v-if="isShowModalTwo">
+  <Modal @close="toggleModalTwo">
+      <h2>Modal 2</h2>
+      <p>Welcome to Modal 2</p>
+  </Modal>
+</teleport>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      isShowModal: false,
+      isShowModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isShowModal = !this.isShowModal;
+    },
+    toggleModalTwo() {
+      this.isShowModalTwo = !this.isShowModalTwo;
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+          <slot></slot>
+          <div>
+            <slot name="link"></slot>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.backdrop {
+    background-color: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    display: flex;
+    align-items: center;
+}
+
+.modal {
+    width: 400px;
+    height: 300px;
+    background-color: white;
+    margin: 0 auto;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    h2 {
+        margin-bottom: 10px;
+    }
+}
+
+</style>
